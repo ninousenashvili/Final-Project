5a2852364127565f23370b8212a3e29f737ed68e
Nino <101824874+ninousenashvili@users.noreply.github.com>
2022-06-07 10:45:22 -0700

    change html name

--- js/Main.js
+++ js/Main.js
@@ -129,3 +129,19 @@ setslide();
 let today = new Date();
 let date = ' ' + today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear();
 document.querySelector(".ftr-bottom-text").append(date);
+
+// burger bar
+
+let hamburger = document.querySelector('.humburger');
+let menubar = document.querySelector('.nav-ul');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('activemenu');
+    menubar.classList.toggle('activemenu');
+
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('activemenu');
+    menubar.classList.remove('activemenu');
+}));
