0e9090973d49418b292ed7fafcaed50a804eca91
Nino <101824874+ninousenashvili@users.noreply.github.com>
2022-06-07 13:35:03 -0700

    add responsive sizes for staff page

--- js/service.js
+++ js/service.js
@@ -68,6 +68,9 @@ document.getElementById('next').addEventListener('click', function () {
 })
 getuser(currentPage);
 
+// current date
+
+
 let today = new Date();
 let date = ' ' + today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear();
 document.querySelector(".ftr-bottom-text").append(date);
